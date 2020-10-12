98b289f0d544b0250deb0c91b045d150c839ac34
Jimiliani <dikorolyov@mail.ru>
2020-10-12 19:38:02 +0300

    МОдели настроены

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 
 from store.models import Item, Order, Tax, Discount
 
+
+class M2MModelAdmin(admin.ModelAdmin):
+    """
+
+    Кастомизация панели заказов
+
+    """
+    filter_horizontal = ['orders', ]
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """
+
+    Кастомизация панели заказов
+
+
+    """
+    filter_horizontal = ['items', ]
+
+
 admin.site.register(Item)
-admin.site.register(Order)
-admin.site.register(Tax)
-admin.site.register(Discount)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Tax, M2MModelAdmin)
+admin.site.register(Discount, M2MModelAdmin)
--- store/models.py
+++ store/models.py
@@ -16,10 +16,10 @@ PRICE_CONDITION_CHOICES = [
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=255, null=False, blank=False)
-    description = models.TextField(null=False, blank=True)
-    price = models.PositiveIntegerField(null=False, blank=False)
-    currency = models.CharField(max_length=3)
+    name = models.CharField(max_length=255, null=False, blank=False, verbose_name='Название')
+    description = models.TextField(null=False, blank=True, verbose_name='Описание')
+    price = models.PositiveIntegerField(null=False, blank=False, verbose_name='Цена')
+    currency = models.CharField(max_length=3, verbose_name='Валюта')
 
     def __str__(self):
         return str(self.name)
@@ -27,12 +27,20 @@ class Item(models.Model):
     def get_absolute_url(self):
         return reverse('store:buy-item', args=[self.id])
 
+    class Meta:
+        verbose_name = 'Вещь'
+        verbose_name_plural = 'Вещи'
+
 
 class Order(models.Model):
-    items = models.ManyToManyField(Item, related_name='orders')
+    items = models.ManyToManyField(Item, related_name='orders', verbose_name='Вещи')
 
     def __str__(self):
         return 'order ' + str(self.id)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
 
 
 class Tax(models.Model):
@@ -42,17 +50,23 @@ class Tax(models.Model):
         (DELIVERY, 'Доставка'),
         (ADDITIONAL_COST, 'НДС')
     ]
-    orders = models.ManyToManyField(Order, related_name='taxes', null=True, blank=True)
-    price = models.PositiveIntegerField()
-    condition_price = models.PositiveIntegerField()
-    keyword = models.CharField(max_length=3, choices=TAXES_CHOICES)
+    orders = models.ManyToManyField(Order, related_name='taxes', blank=True, verbose_name='Заказы')
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    condition_price = models.PositiveIntegerField(verbose_name='Цена для условия')
+    keyword = models.CharField(max_length=3, choices=TAXES_CHOICES, verbose_name='Вид налога')
     order_price_condition = models.CharField(
+        verbose_name='Оператор условия',
         help_text='Условный оператор, который должен выполниться, для использования этого налога, например,'
                   ' если в этом поле указан знак ">", то если "цена заказа" > "цена налога", то налог активируется'
-                  ' относительно данного заказа', max_length=3, choices=PRICE_CONDITION_CHOICES)
+                  ' относительно данного заказа', max_length=3,
+        choices=PRICE_CONDITION_CHOICES)
 
     def __str__(self):
         return self.keyword + ' ' + str(self.price)
+
+    class Meta:
+        verbose_name = 'Налог'
+        verbose_name_plural = 'Налоги'
 
 
 class Discount(models.Model):
@@ -62,14 +76,19 @@ class Discount(models.Model):
         (FREE_DELIVERY, 'Бесплатная доставка'),
         (BIG_ORDER, 'Большой заказ')
     ]
-    orders = models.ManyToManyField(Order, related_name='discounts', null=True, blank=True)
-    price = models.PositiveIntegerField()
-    condition_price = models.PositiveIntegerField()
-    keyword = models.CharField(max_length=3, choices=DISCOUNT_CHOICES)
+    orders = models.ManyToManyField(Order, related_name='discounts', blank=True, verbose_name='Заказы')
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    condition_price = models.PositiveIntegerField(verbose_name='Цена для условия')
+    keyword = models.CharField(max_length=3, choices=DISCOUNT_CHOICES, verbose_name='Вид налога')
     order_price_condition = models.CharField(
+        verbose_name='Оператора условия',
         help_text='Условный оператор, который должен выполниться, для использования этой скидки, например,'
                   ' если в этом поле указан знак ">", то если "цена заказа" > "цена скидки", то скидка активируется'
                   ' относительно данного заказа', max_length=3, choices=PRICE_CONDITION_CHOICES)
 
     def __str__(self):
         return self.keyword + ' ' + str(self.price)
+
+    class Meta:
+        verbose_name = 'Скидка'
+        verbose_name_plural = 'Скидки'
